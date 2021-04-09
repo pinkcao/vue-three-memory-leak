c13d77b1457f22ce712e31b6aea9253cd45a83f9
pinkcao <972558143@qq.com>
2021-04-09 15:14:18 +0800

    delete part of code to create a minimal demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="container" id="container" ref="container" @click="onMouseClick">
-    <div v-if="mode == 'design'" class="compose-button">
-      <el-button @click.stop.prevent="composeMesh">绑定</el-button>
-    </div>
-    <div v-if="mode == 'design'" class="discompose-button">
-      <el-button @click.stop.prevent="discomposeGroup">解绑</el-button>
-    </div>
+  <div class="container" id="container" ref="container">
     <div class="route-button">
       <el-button @click.stop.prevent="routes">route</el-button>
     </div>
@@ -16,11 +10,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-// import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
-// import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
-// import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js'
-// import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-// import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -31,15 +20,8 @@ export default {
     return {
       group: null,
       objects: [],
-      effectFXAA: null,
-      // outlinePass: null,
       pixelRatio: null,
-      // fxaaPass: null,
-      mouse: new THREE.Vector2(),
-      dbmouse: new THREE.Vector2(),
-      raycaster: new THREE.Raycaster(),
       selectedObjects: [],
-      // composer: null,
       stats: null,
       animationFrame: null,
       directionalLight: null,
@@ -86,11 +68,9 @@ export default {
       this.initScene()
       this.initCamera()
       this.initRenderer()
-      // this.initGeometry()
       this.initLoader()
       this.initLight()
       this.initControls()
-      // this.initComposer()
       this.initStats()
     },
 
@@ -107,32 +87,23 @@ export default {
       for (let i = 0; i < this.atomicArr.length; i++) {
         this.atomicArr[i].geometry.dispose()
         this.atomicArr[i].material.dispose()
-        // this.atomicArr[i].geometry = null
-        // this.atomicArr[i].material = null
         console.log(this.atomicArr[i])
       }
       this.controls.dispose()
       this.controls = null
       this.atomicArr = null
       this.groupMap = null
-      // console.log(this.groupMap)
       this.light = null
       this.loader = null
-      // console.log('all stuffs reset')
     },
 
-    //初始化透视摄像机，此外还有正交摄像机
-    //那是不是甚至可以模拟摄像机，比如说在环境中加入n个摄像机，每个摄像机有其vector3坐标，然后lookat某个点，然后双击后切换到该摄像机看到的事件？
     initCamera() {
       this.camera = new THREE.PerspectiveCamera(65, this.width / this.height, 0.1, 10000)
       this.camera.position.set(5, 5, 30)
     },
-
-    //初始化scene
     initScene() {
       this.scene = new THREE.Scene()
     },
-    //初始化光照
     initLight() {
       // let directionalLight = track(new THREE.DirectionalLight(0xffffff, 2)) //平行光源
       let directionalLight = new THREE.DirectionalLight(0xffffff, 2)
@@ -229,28 +200,6 @@ export default {
         }
       )
     },
-    // initComposer() {
-    //   // if (this.renderer != null) {
-    //   this.composer = new EffectComposer(this.renderer)
-    //   let renderPass = new RenderPass(this.scene, this.camera)
-    //   this.fxaaPass = new ShaderPass(FXAAShader)
-    //   const pixelRatio = this.renderer.getPixelRatio()
-    //   this.fxaaPass.material.uniforms['resolution'].value.x = 1 / (this.width * pixelRatio)
-    //   this.fxaaPass.material.uniforms['resolution'].value.y = 1 / (this.height * pixelRatio)
-    //   this.composer.addPass(renderPass)
-    //   this.composer.addPass(this.fxaaPass)
-    //   this.outlinePass = new OutlinePass(new THREE.Vector2(this.width, this.height), this.scene, this.camera)
-    //   this.outlinePass.edgeStrength = 3 //包围线浓度
-    //   this.outlinePass.edgeGlow = 1 //边缘线范围
-    //   this.outlinePass.edgeThickness = 1 //边缘线浓度
-    //   this.outlinePass.pulsePeriod = 2 //包围线闪烁频率
-    //   this.outlinePass.visibleEdgeColor.set('#00ffff') //包围线颜色
-    //   this.outlinePass.hiddenEdgeColor.set('#190a05') //被遮挡的边界线颜色
-    //   this.outlinePass.renderToScreen = true
-    //   this.composer.addPass(this.outlinePass)
-    //   // }
-    // },
-
     initStats() {
       // if (this.$refs.container != undefined) {
       this.stats = new Stats()
@@ -286,114 +235,12 @@ export default {
       this.render()
     },
 
-    onMouseClick(event) {
-      console.log(this.scene)
-      console.log(event.offsetX)
-      console.log(this.$refs.container.getBoundingClientRect())
-      this.mouse.x = (event.offsetX / (this.width * (1 / 1))) * 2 - 1
-      this.mouse.y = -(event.offsetY / (this.height * (1 / 1))) * 2 + 1
-
-      this.raycaster.setFromCamera(this.mouse, this.camera)
-
-      var intersects = this.raycaster.intersectObjects(this.scene.children, true)
-
-      //当选中了确切的物体时
-      if (intersects.length > 0) {
-        console.log(intersects)
-        let tempStore = intersects[0].object
-        //推回至最上层的父结点，选中最上层的这个父结点
-        // while (tempStore.parent.type != 'Scene') {
-        //   tempStore = tempStore.parent
-        // }
-        let tempuuid = tempStore.userData.uuid
-        //选中的物体uuid == -1时
-        if (tempuuid == -1) {
-          //如果当前未选中该物体
-          if (this.selectedObjects.indexOf(tempStore) < 0) {
-            // let tempGroupArr = this.groupMap.get(tempuuid)
-            this.selectedObjects.push(tempStore)
-          }
-        }
-        if (tempuuid != -1) {
-          //如果当前未选中该物体
-          if (this.selectedObjects.indexOf(tempStore) < 0) {
-            let tempGroupArr = this.groupMap.get(tempuuid)
-            for (let i = 0; i < tempGroupArr.length; i++) {
-              this.selectedObjects.push(tempGroupArr[i])
-            }
-          }
-          //如果当前已选中该物体
-          else if (this.selectedObjects.indexOf(tempStore) >= 0) {
-            let tempGroupArr = this.groupMap.get(tempuuid)
-            this.selectedObjects.splice(this.selectedObjects.indexOf(tempStore), 1)
-            for (let i = 0; i < tempGroupArr.length; i++) {
-              this.selectedObjects.push(tempGroupArr[i])
-            }
-          }
-        }
-        console.log(this.selectedObjects)
-        // this.outlinePass.selectedObjects = this.selectedObjects
-      }
-
-      if (intersects.length == 0) {
-        console.log('nothing selected')
-        this.selectedObjects = []
-        // this.outlinePass.selectedObjects = []
-      }
-    },
-    /**
-     * 把需要组合的mesh的userData.uuid赋为同一值，通过该值将所有的mesh联系起来
-     */
-    composeMesh() {
-      console.log(this.selectedObjects)
-      //如果被选中物体有2个及以上
-      if (this.selectedObjects.length > 1) {
-        //新建一个group
-        let tempGroup = new THREE.Group()
-        //从场景中删除所有被选中的物体，将被删除的物体添加入组中
-        //2021.4.6，不再删除场景中被选中的物体，转而将被选中的物体设置uuid，作为compose的结果
-        let tempuuid = uuidv4()
-        let tempGroupArr = []
-        //浅拷贝一份当前选中的东西的引用,并且把uuid赋给每个selectedObject，解绑时把uuid赋为-1
-        for (let i = 0; i < this.selectedObjects.length; i++) {
-          tempGroupArr.push(this.selectedObjects[i])
-          this.selectedObjects[i].userData.uuid = tempuuid
-        }
-        //在map中添加当前tempGroupArr，方便查找
-        this.groupMap.set(tempuuid, tempGroupArr)
-        this.selectedObjects = []
-        // this.outlinePass.selectedObjects = []
-      }
-    },
-    /**
-     * mesh.userData.uuid 值赋为-1，解绑
-     */
-    discomposeGroup() {
-      //当选中的mesh是>=2个时才有删除必要
-      if (this.selectedObjects.length > 1) {
-        let tempSet = new Set()
-        for (let i = 0; i < this.selectedObjects.length; i++) {
-          tempSet.add(this.selectedObjects[i].userData.uuid)
-          this.selectedObjects[i].userData.uuid = -1
-        }
-        for (let arruuid of tempSet) {
-          this.groupMap.delete(arruuid)
-        }
-      }
-      this.selectedObjects = []
-      // this.outlinePass.selectedObjects = []
-    },
-    //深度遍历树，把所有叶子结点添加入数组中
+    //DFS tree, add all leaf node into atmoicArr
     DFS(node, nodeList) {
       // console.log(node)
       if (node) {
         if (node.children.length == 0) {
           nodeList.push(node)
-          // this.geometry.push(node.geometry)
-          // this.material.push(node.material)
-          // node.geometry.dispose()
-          // node.material.aoMap.dispose()
-          // node.material.dispose()
         } else if (node.children.length > 0) {
           let children = node.children
           for (var i = 0; i < children.length; i++) {
